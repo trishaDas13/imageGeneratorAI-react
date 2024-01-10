bd6534a0ce54c84b3ed296484702b91fe58ea15b
trishaDas13 <trisha.das1308@gmail.com>
2024-01-10 10:52:20 +0530

    dark mode applied

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
+
 function Generator() {
 
-    const [text, setText] = useState();
+    const [text, setText] = useState('');
     const [imageURL, setImageURL] = useState('');
+    const[changeMode, setChangeMode] = useState(false);
 
     const API_TOKEN = "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";
 
+    //todo: fetch and append image
     function generateImage(){
         async function query(data) {
             const response = await fetch(
@@ -20,15 +23,24 @@ function Generator() {
             const result = await response.blob();
             return result;
         }
+        if (text === ""){
+           alert("Please enter some text to generate an image!") ;
+           return;        
+        }
         query({"inputs": text}).then((response) => {
             const Url = URL.createObjectURL(response);
             setImageURL(Url);
         });
     }
 
+    //todo: toggle between light and dark mode
+    function toggleMode(){
+        setChangeMode(!changeMode);
+    }
+
   return (
-    <div>
-        <p>Image Generation App</p>
+    <>
+        <h2>Image Generation App</h2>
         <div className="searchBar">
         <input type="text" 
             onChange={(e) => setText(e.target.value)}
@@ -36,10 +48,16 @@ function Generator() {
         <button onClick={generateImage}>Generate Image!</button>
         </div>
 
-        <div className="generatorContainer">
-           <img src={imageURL} alt="" />
+        <div 
+            className={changeMode ? 'darkmode' : 'generatorContainer'}
+        >
+           <img src={imageURL} alt="" width="350px" />
+            <span 
+                onClick={toggleMode}
+            >{changeMode ? '🌞' : '🌙'}</span>
         </div>
-    </div>
+        
+    </>
   )
 }
 
